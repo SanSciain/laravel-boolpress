f99528e8ff05975f19c683750e4ae6a7b8b38826
SanSciain <mastroeni.santi@gmail.com>
2022-07-05 18:08:24 +0200

    Add: FrontOffice

---
+++ resources/js/components/FirstComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Work in progress</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FirstComponent",
+};
+</script>
+
+<style></style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>FrontOffice</h1>
+        <FirstComponent />
+    </div>
+</template>
+
+<script>
+import FirstComponent from "../components/FirstComponent.vue";
+export default {
+    name: "App",
+    components: {
+        FirstComponent,
+    },
+};
+</script>
+
+<style></style>
